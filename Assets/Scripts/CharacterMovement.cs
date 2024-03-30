0669da13695fdf8facd0b800ae592b242f6e1640
James Bridges <jb00656@my.westga.edu>
2024-03-30 19:19:01 -0400

    Added Character Controller
    implemented movement with W (Forward), A(Strafe Left), S(Backward), D(Strafe Right)
    implemented looking with mouse movement

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -12,9 +12,10 @@ using UnityEngine.InputSystem;
 public class CharacterMovement : MonoBehaviour
 {
     private CharacterController controller;
-    public float movementSpeed = 2.0f;
+    public float movementSpeed = 10.0f;
 
-    private float direction;
+    private float forwardDirection;
+    private float strafeDirection;
 
     // Start is called before the first frame update
     void Start()
@@ -25,12 +26,15 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 adjustedDirection = transform.forward * forwardDirection * movementSpeed * Time.deltaTime;
+        adjustedDirection += transform.right * Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        controller.Move(adjustedDirection);
     }
 
     private void OnMove(InputValue value)
     {
         Vector2 controls = value.Get<Vector2>();
-
+        forwardDirection = controls.y;
     }
 }
+
